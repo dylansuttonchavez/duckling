7157df2773468bf46a36944a14aaf66a6e507de4
Dylan Sutton Chavez <dylan718281@gmail.com>
2025-06-16 14:58:53 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+import stripe
+import os
+
+stripe.api_key = os.environ.get("STRIPE_SECRET_KEY")
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "stripe_public_key": os.environ.get("STRIPE_PUBLIC_KEY")
+    })
+
+@app.post("/create-checkout-session")
+def create_checkout_session():
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price_data': {
+                'currency': 'mxn',
+                'product_data': {'name': 'Curso Intensivo Spyware'},
+                'unit_amount': 22300,
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url='https://tu-dominio.com/success',
+        cancel_url='https://tu-dominio.com/cancel',
+    )
+    return {"sessionId": session.id}
+
+# python -m uvicorn main:app --reload
+
+# python -m uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4
