05e44f852ae1362c4fd13a16d8b7c2c7d85a8e66
Dylan Sutton Chavez <dylan718281@gmail.com>
2025-06-18 04:21:36 +0100

    Delete config.sql

--- config.sql
+++
@@ -1,6 +0,0 @@
-create table access_sessions (
-    id uuid primary key default gen_random_uuid(),
-    session_id text unique not null,
-    used boolean not null default true,
-    created_at timestamptz default now()
-);
